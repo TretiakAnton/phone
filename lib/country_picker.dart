bc2048a1b6cd5f19d4dff4b0341fdbd0b84d1c02
TretiakAnton <anton.v.tretyak@gmail.com>
2022-10-13 19:09:26 +0300

    start

---
+++ lib/country_picker.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Widget _buildDropdownItem(Country country) => Container(
+      child: Row(
+        children: <Widget>[
+          CountryPickerUtils.getDefaultFlagImage(country),
+          SizedBox(
+            width: 8.0,
+          ),
+          Text("+${country.phoneCode}(${country.isoCode})"),
+        ],
+      ),
+    );
---
+++ lib/models/country.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Country {
+  Country(name, url, code);
+  String name = '';
+  String url = '';
+  int code = 0;
+  Widget flagImage = progresci;
+}
